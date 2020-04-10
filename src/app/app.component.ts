858d0f5bd4838655614a4695173bdef6c108ff62
Wojciech Dudek <wdudek82@gmail.com>
2020-04-11 01:22:27 +0200

    Remove unused imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { ForecastService } from './weather/forecast.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
